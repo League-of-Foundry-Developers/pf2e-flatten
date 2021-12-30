ba97cc62f84e4cd640f1688ec7a28f19853e9b89
velara <74130268+velara@users.noreply.github.com>
2021-12-29 22:10:57 -0600

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -28,10 +28,6 @@ Hooks.once("init", () => {
 Hooks.on('createActor', AutoFlattenNPC );
 Hooks.on('getActorDirectoryEntryContext', onFlattenProficiencyContextHook);
 
-async function retrivePWL(){
-	return await game.settings.get(settingsKey, "halflevel") ? game.packs.get('pf2e-flatten.pf2e-proficiency-without-level').getDocument(game.packs.get('pf2e-flatten.pf2e-proficiency-without-level').index.find(e => e.name === "1/2 PWL")._id) : game.packs.get('pf2e-flatten.pf2e-proficiency-without-level').getDocument(game.packs.get('pf2e-flatten.pf2e-proficiency-without-level').index.find(e => e.name === "PWL")._id);
-}
-
 async function AutoFlattenNPC(li){
  if(game.settings.get(settingsKey, "autoflatten") === true){
 	 	const modifierName = game.settings.get(settingsKey, "halflevel") ? '1/2 Level Proficiency' :'Proficiency Without Level';
@@ -46,13 +42,14 @@ async function AutoFlattenNPC(li){
 
 async function onFlattenProficiencyContextHook(html, buttons) {
 const modifierName = game.settings.get(settingsKey, "halflevel") ? '1/2 Level Proficiency' :'Proficiency Without Level';
-const modifierUnName = game.settings.get(settingsKey, "halflevel") ? 'Proficiency Without Level': '1/2 Level Proficiency' ;
+const modifierUnSlug = game.settings.get(settingsKey, "halflevel") ? 'proficiency-without-level': '1/2-level-proficiency' ;
+const modifierSlug = game.settings.get(settingsKey, "halflevel") ? '1/2-level-proficiency' : 'proficiency-without-level'
 const hasModifier = (actor) => {
 		const data = actor.data.data;
-		if (data['customModifiers'] && data['customModifiers'].all) {
-				const all = data['customModifiers'].all;
+		if (data.customModifiers && data.customModifiers.all) {
+				const all = data.customModifiers.all;
 				for (const modifier of all) {
-						if (modifier.name === modifierName) {
+						if (modifier.label === modifierName) {
 								return true;
 						}
 				}
@@ -63,12 +60,12 @@ const hasModifier = (actor) => {
 				name: 'PF2e Flatten NPC',
 				icon: '<i class="fas fa-level-down-alt"></i>',
 				condition: (li) => {
-						const id = li.data('entity-id');
+						const id = li.data('document-id');
 						const actor = game.actors.get(id);
-						return actor.data.type === 'npc' && !hasModifier(actor);
+						return actor?.data.type === 'npc' && !hasModifier(actor);
 				},
 				callback: async (li) => {
-						const id = li.data('entity-id');
+						const id = li.data('document-id');
 						const actor = game.actors.get(id);
 						const level = game.settings.get(settingsKey, "halflevel") ? Math.max(Math.floor(parseInt(actor?.data.data['details'].level.value)/2), 0) : Math.max(parseInt(actor?.data.data['details'].level.value),0);
 						await actor.addCustomModifier('all', modifierName, -level, 'untyped');
@@ -79,15 +76,14 @@ const hasModifier = (actor) => {
 				name: 'PF2e Unflatten NPC',
 				icon: '<i class="fas fa-level-up-alt"></i>',
 				condition: (li) => {
-						const id = li.data('entity-id');
+						const id = li.data('document-id');
 						const actor = game.actors.get(id);
-						return actor.data.type === 'npc' && hasModifier(actor);
+						return actor?.data.type === 'npc' && hasModifier(actor);
 				},
 				callback: async (li) => {
-						const id = li.data('entity-id');
+						const id = li.data('document-id');
 						const actor = game.actors.get(id);
-						await actor.removeCustomModifier('all', modifierName);
+						await actor.removeCustomModifier('all', modifierSlug);
 				},
 		});
 		}
-
